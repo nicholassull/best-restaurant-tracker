4ebabfa5b14a91e2d25e07e48bce894a8d4b74b9
Nick Sullivan <nicholassull@gmail.com>
2022-03-06 11:45:12 -0800

    add initial files

---
+++ BestRestaurants/Models/FriendLetterContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+namespace BestRestaurants.Models
+{
+  public class BestRestaurantsContext : DbContext
+  {
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Cuisine> Cuisines { get; set; }
+    public BestRestaurantsContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
