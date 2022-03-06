f4128424d3ce40e853fb39a95efc66305a4194df
Nick Sullivan <nicholassull@gmail.com>
2022-03-06 13:46:30 -0800

    add form for creating new cuisine

---
+++ BestRestaurants/Controllers/CuisinesController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using BestRestaurants.Models;
+
+namespace BestRestaurants.Controllers
+{
+  public class CuisinesController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
+
+    public CuisinesController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Cuisine> model = _db.Cuisines.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Cuisine cuisine)
+    {
+      _db.Cuisines.Add(cuisine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
--- BestRestaurants/Models/BestRestaurantsContext.cs
+++ BestRestaurants/Models/BestRestaurantsContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+
 namespace BestRestaurants.Models
 {
   public class BestRestaurantsContext : DbContext
