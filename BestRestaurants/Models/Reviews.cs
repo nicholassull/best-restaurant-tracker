4f390462e7ed2d5946af9470703f5e4929cd87ca
Nick Sullivan <nicholassull@gmail.com>
2022-03-08 19:29:58 -0800

    improve buttons on Index pages

---
+++ BestRestaurants/Models/Reviews.cs
@@ -0,0 +1,10 @@
+
+namespace BestRestaurants.Models
+{
+  public class Review
+  {
+    public int Stars { get; set; }
+    public string User { get; set; }
+    public string Text { get; set; }
+  }
+}
