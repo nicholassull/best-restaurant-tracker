552b2fef784de357c2682c97b90bdc2d7a995591
Nick Sullivan <nicholassull@gmail.com>
2022-03-08 19:46:59 -0800

    create Reviews class

--- BestRestaurants/Models/Reviews.cs
+++ BestRestaurants/Models/Reviews.cs
@@ -6,5 +6,7 @@ namespace BestRestaurants.Models
     public int Stars { get; set; }
     public string User { get; set; }
     public string Text { get; set; }
+    public int RestaurantId { get; set; }
+    // public virtual Cuisine Cuisine { get; set; }
   }
 }
