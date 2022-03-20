476499ca225d8e30e4513d829624da41fd8ce09e
Nick Sullivan <nicholassull@gmail.com>
2022-03-20 10:17:30 -0700

    add DesignTimeDbContext and remove reference to reviews

---
+++ BestRestaurants/Models/DesignTimeDbContext.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace BestRestaurants.Models
+{
+  public class BestRestaurantsContextFactory : IDesignTimeDbContextFactory<UniversityRegistrarContext>
+  {
+
+    BestRestaurantsContext IDesignTimeDbContextFactory<UniversityRegistrarContext>.CreateDbContext(string[] args)
+    {
+      IConfigurationRoot configuration = new ConfigurationBuilder()
+          .SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json")
+          .Build();
+
+      var builder = new DbContextOptionsBuilder<BestRestaurantsContext>();
+
+      builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(configuration["ConnectionStrings:DefaultConnection"]));
+
+      return new BestRestaurantsContext(builder.Options);
+    }
+  }
+}
